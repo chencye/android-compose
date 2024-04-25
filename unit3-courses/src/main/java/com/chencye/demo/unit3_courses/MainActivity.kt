d6f4c20b5e2a2c9cd4278306be7ae684b85bba16
ccy <chencye@gmail.com>
2024-04-25 02:38:17 +0000

    unit3-courses (#10)
    Co-authored-by: chenchengye01 <chenchengye01@countrygarden.com.cn>
    Reviewed-on: https://tea.889800.xyz/ccy/android-compose/pulls/10

--- unit3-courses/src/main/java/com/chencye/demo/unit3_courses/MainActivity.kt
+++ unit3-courses/src/main/java/com/chencye/demo/unit3_courses/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,6 +28,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.chencye.demo.unit3_courses.data.DataSource
 import com.chencye.demo.unit3_courses.ui.theme.CoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,60 +50,71 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CoursesApp() {
-
+    CoursesList(topicList = DataSource.topics)
 }
 
 @Composable
 fun CoursesList(topicList: List<Topic>, modifier: Modifier = Modifier) {
-
-}
-
-@Composable
-fun CourseCard(topic: Topic, modifier: Modifier = Modifier) {
-    Row {
-        Image(
-            painter = painterResource(id = topic.pic),
-            contentDescription = stringResource(id = topic.title),
-            modifier = Modifier
-                .height(68.dp)
-                .width(68.dp),
-            contentScale = ContentScale.Crop
-        )
-        Column(modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)) {
-            Text(
-                text = stringResource(id = topic.title),
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Row(modifier = Modifier.padding(top = 8.dp)) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_grain),
-                    contentDescription = null,
-                )
-                Text(
-                    text = topic.amount.toString(),
-                    modifier = Modifier
-                        .padding(start = 8.dp)
-                        .align(Alignment.CenterVertically),
-                    style = MaterialTheme.typography.labelMedium
-                )
-            }
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.padding(8.dp)
+    ) {
+        items(topicList) { topic ->
+            CourseCard(topic)
         }
     }
 }
 
-// @Preview(showBackground = true)
 @Composable
-fun CoursesAppPreview() {
-    CoursesTheme {
-        CoursesApp()
+fun CourseCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Row {
+            Image(
+                painter = painterResource(id = topic.pic),
+                contentDescription = stringResource(id = topic.title),
+                modifier = Modifier
+                    .height(68.dp)
+                    .width(68.dp)
+                    .aspectRatio(1f),
+                contentScale = ContentScale.Crop
+            )
+            Column(modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)) {
+                Text(
+                    text = stringResource(id = topic.title),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Row(modifier = Modifier.padding(top = 8.dp)) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                    )
+                    Text(
+                        text = topic.amount.toString(),
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                            .align(Alignment.CenterVertically),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun CourseCardPreview() {
     CoursesTheme {
         CourseCard(Topic(R.string.architecture, 58, R.drawable.architecture))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CoursesAppPreview() {
+    CoursesTheme {
+        CoursesApp()
     }
 }
