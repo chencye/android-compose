69ea2e1ab254c385a42be8537e9ec9ebc0f4c68a
无钟声有 <chencye@163.com>
2024-04-26 10:38:06 +0800

    unit4-dessert-clicker lifecycle (#3)
    Co-authored-by: chenchengye01 <chenchengye01@countrygarden.com.cn>

---
+++ unit4-dessert-clicker/src/main/java/com/chencye/demo/unit4_dessert_clicker/model/Dessert.kt
@@ -0,0 +1,4 @@
+package com.chencye.demo.unit4_dessert_clicker.model
+
+
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
---
+++ unit4-unscramble/src/main/java/com/chencye/demo/unit4_unscramble/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.chencye.demo.unit4_unscramble
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.chencye.demo.unit4_unscramble.ui.GameScreen
+import com.chencye.demo.unit4_unscramble.ui.theme.UnscrambleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnscrambleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    GameScreen()
+                }
+            }
+        }
+    }
+}
