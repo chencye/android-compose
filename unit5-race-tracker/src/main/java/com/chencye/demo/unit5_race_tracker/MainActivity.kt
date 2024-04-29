e32a5f7447f1b03955e6a5769f15b96d0969656b
无钟声有 <chencye@163.com>
2024-04-29 15:31:16 +0800

    unit5-race-tracker starter (#9)
    Co-authored-by: chenchengye01 <chenchengye01@countrygarden.com.cn>

---
+++ unit5-race-tracker/src/main/java/com/chencye/demo/unit5_race_tracker/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.chencye.demo.unit5_race_tracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.chencye.demo.unit5_race_tracker.ui.RaceTrackerApp
+import com.chencye.demo.unit5_race_tracker.ui.theme.RaceTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RaceTrackerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    RaceTrackerApp()
+                }
+            }
+        }
+    }
+}
