f5ecd3063c30cf2be512db65275ac2124d78bcc2
无钟声有 <chencye@163.com>
2024-04-29 17:08:29 +0800

    连接互联网获取数据 (#12)
    Co-authored-by: chenchengye01 <chenchengye01@countrygarden.com.cn>

--- unit5-mars-photos/build.gradle.kts
+++ unit5-mars-photos/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.retrofit2)
+    implementation(libs.retrofit2.converter.scalars)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
