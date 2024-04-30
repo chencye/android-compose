4c8fb68daf128ed310e138a8b6726cb270caa9ee
chenchengye01 <chenchengye01@countrygarden.com.cn>
2024-04-30 11:16:51 +0800

    Dependency Injection

--- README.md
+++ README.md
@@ -214,3 +214,7 @@ from the internet using Retrofit. Then use the Coil library to display images in
 - viewModelScope
 - INTERNET permission
 - serialization
+
+Dependency Injection
+![](https://developer.android.com/static/codelabs/basic-android-kotlin-compose-add-repository/img/ff7a7cd039402747_856.png)
+
--- unit5-mars-photos/src/main/java/com/chencye/demo/unit5_mars_photos/ui/screens/MarsViewModel.kt
+++ unit5-mars-photos/src/main/java/com/chencye/demo/unit5_mars_photos/ui/screens/MarsViewModel.kt
@@ -5,8 +5,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
-import com.chencye.demo.unit5_mars_photos.network.MarsApi
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.chencye.demo.unit5_mars_photos.MarsPhotosApplication
+import com.chencye.demo.unit5_mars_photos.data.MarsPhotosRepository
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
@@ -17,7 +22,7 @@ sealed interface MarsUiState {
     object Loading : MarsUiState
 }
 
-class MarsViewModel : ViewModel() {
+class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : ViewModel() {
 
     /** The mutable State that stores the status of the most recent request */
     var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
@@ -37,7 +42,7 @@ class MarsViewModel : ViewModel() {
     fun getMarsPhotos() {
         viewModelScope.launch {
             marsUiState = try {
-                val listResult = MarsApi.retrofitService.getPhotos()
+                val listResult = marsPhotosRepository.getMarsPhotos()
                 MarsUiState.Success("Success: ${listResult.size} Mars photos retrieved")
             } catch (e: IOException) {
                 Log.e("getMarsPhotos", "IOException: ${e.message}")
@@ -45,6 +50,19 @@ class MarsViewModel : ViewModel() {
             } catch (e: HttpException) {
                 Log.e("getMarsPhotos", "HttpException: ${e.message}")
                 MarsUiState.Error
+            }
+        }
+    }
+
+    // 因为 Android 框架不允许 ViewModel 在创建时在构造函数中传递值
+    // 所以实现 ViewModelProvider.Factory 对象，以绕过这个限制。
+    // 伴生对象通过拥有每个人都可以使用的单个对象实例，而无需创建昂贵对象的新实例
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as MarsPhotosApplication)
+                val marsPhotosRepository = application.container.marsPhotosRepository
+                MarsViewModel(marsPhotosRepository = marsPhotosRepository)
             }
         }
     }
