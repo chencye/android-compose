1b95ee85e6ef4fb5882fa94485062c3cd5f41c8a
无钟声有 <chencye@163.com>
2024-04-30 15:07:04 +0800

    利用coil库加载互联网图片 (#15)
    Co-authored-by: chenchengye01 <chenchengye01@countrygarden.com.cn>

--- unit5-mars-photos/build.gradle.kts
+++ unit5-mars-photos/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit2.converter.kotlinx)
     implementation(libs.okhttp)
+    implementation(libs.coil)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
